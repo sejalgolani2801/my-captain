7e40f7fc4e2c9a9d179d9627ed0273149973d7c0
sejalgolani2801 <69432929+sejalgolani2801@users.noreply.github.com>
2020-08-23 15:20:47 +0530

    Add files via upload
    to find extension of the input file

---
+++ program2.py
@@ -0,0 +1,13 @@
+a=input("enter file ")
+if ".py" in a :
+    print("The extension of the file is: python")
+elif ".txt" in a:
+    print("The extension of the file is:txt")
+elif ".java" in a:
+    print("The extension of the file is:java")
+elif ".c" in a:
+    print("The extension of the file is:c")
+elif ".html" in a:
+    print("The extension of the file is:.html")
+else:
+    print("enter valid extension")
